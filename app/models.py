acf2d826075e2fc44b2b939331dd2abab2d8d295
CodingMinh <87570399+CodingMinh@users.noreply.github.com>
2025-06-28 16:44:29 +0700

    finished the flask mega-tutorial

--- app/models.py
+++ app/models.py
@@ -1,16 +1,17 @@
 """ database model/schema/structure """
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
-from flask import current_app
+from flask import current_app, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
 import jwt, json, redis, rq
 from app.search import add_to_index, remove_from_index, query_index
+import secrets
 
 """ 
 act as a "glue" layer between the SQLAlchemy and Elasticsearch worlds, 
@@ -73,6 +74,27 @@ before and after each commit respectively
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 
+class PaginatedAPIMixin(object):
+    """ converts a paginated SQLAlchemy query result (e.g. a list of User or Post objects) into a Python dictionary """
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = db.paginate(query, page=page, per_page=per_page, error_out=False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page, **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
 """
 - use columns because the association table looks sth like this
            followers
@@ -93,7 +115,7 @@ followers = sa.Table(
     sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'), primary_key=True)
 )
 
-class User(UserMixin, db.Model):
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -116,6 +138,8 @@ class User(UserMixin, db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     last_message_read_time: so.Mapped[Optional[datetime]]
+    token: so.Mapped[Optional[str]] = so.mapped_column(sa.String(32), index=True, unique=True)
+    token_expiration: so.Mapped[Optional[datetime]]
 
     # secondary is the association table
     # primaryjoin is the condition that links our side to the association table
@@ -269,6 +293,61 @@ class User(UserMixin, db.Model):
     def get_task_in_progress(self, name):
         query = self.tasks.select().where(Task.name == name, Task.complete == False)
         return db.session.scalar(query)
+    
+    def posts_count(self):
+        query = sa.select(sa.func.count()).select_from(self.posts.select().subquery())
+        return db.session.scalar(query)
+    
+    """ converts a model instance (e.g. User or Post) into a Python representation (dictionary), which will then be converted to JSON """
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'last_seen': self.last_seen.replace(tzinfo=timezone.utc).isoformat() if self.last_seen else None,
+            'about_me': self.about_me,
+            'posts_count': self.posts_count(),
+            'followers_count': self.followers_count(),
+            'following_count': self.following_count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'followers': url_for('api.get_followers', id=self.id),
+                'following': url_for('api.get_following', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+    
+    """ takes a Python dictionary and updates the attributes of a model instance """
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                # built-in python function, sets the self object's field attribute to data[field] value dynamically
+                # e.g. sets self's username field to data[username] value dynamically
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
+
+    """ generate a temporary token for API authentication purposes """
+    def get_token(self, expires_in=3600):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration.replace(tzinfo=timezone.utc) > now + timedelta(seconds=60):
+            return self.token
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+    
+    def revoke_token(self):
+        self.token_expiration = datetime.now(timezone.utc) - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = db.session.scalar(sa.select(User).where(User.token == token))
+        if user is None or user.token_expiration.replace(tzinfo=timezone.utc) < datetime.now(timezone.utc):
+            return None
+        return user
 
 @login.user_loader
 def load_user(id): # id passed in here is string so we want to convert back to int for our database
